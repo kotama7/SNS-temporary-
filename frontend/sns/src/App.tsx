a6ff118c28ec9516ab6b90ef7a9f6f9c976a4af8
kotama7 <kotamatakanori2@gmail.com>
2024-02-13 23:37:23 +0900

    add:http環境作成

--- frontend/sns/src/App.tsx
+++ frontend/sns/src/App.tsx
@@ -12,16 +12,11 @@ function App() {
     session: session,
     setSession: setSession
   }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <LoginForm {...session_params}/>
-        <HOME session={session}/>
-      </header>
+      <LoginForm {...session_params}/>
+      <HOME session={session}/>
     </div>
   );
 }

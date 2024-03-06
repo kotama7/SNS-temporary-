94f47a39c273d18f1db8ddec0cde54f3c1255544
michi0008 <michi322418@gmail.com>
2024-03-06 23:35:07 +0900

    add InfinityScroll

--- frontend/sns/src/home/timeline.tsx
+++ frontend/sns/src/home/timeline.tsx
@@ -2,27 +2,28 @@ import React from 'react';
 import {AtpSessionData, BskyAgent} from '@atproto/api';
 import { useState, useEffect } from 'react';
 import './timeline.css';
+import InfiniteScroll from "react-infinite-scroller";
 
-interface TimeLineProps {
+export interface TimeLineProps {
     // タイムラインの内容
     tweets: TweetsInfo[];
     // 取得したツイートの数
     tweets_num: number;
 }
 
-interface TweetsInfo {
+export interface TweetsInfo {
     // ツイートの情報
     tweet: string;
     user: string;
 }
 
-interface TimeLineInfo {
+export interface TimeLineInfo {
     // タイムラインの情報
     session: AtpSessionData;
     load_num: number;
 }
 
-function Timeline(props: {session: AtpSessionData}){
+export function Timeline(props: TimeLineInfo): JSX.Element{
     const [load_num, setLoadNum] = useState<number>(10);
     // タイムラインの内容を取得する
     const timeline: TimeLineProps = getTimeline({
@@ -40,14 +41,12 @@ function Timeline(props: {session: AtpSessionData}){
 
     return (
         <div>
-            <TimelineForward load_num={load_num} setLoadNum={setLoadNum}/>
             {/* タイムラインの内容 */}
-            <TimelineBackward load_num={load_num} setLoadNum={setLoadNum}/>
         </div>
     )
 }
 
-function getTimeline(props: TimeLineInfo){
+function getTimeline(props: TimeLineInfo): TimeLineProps {
 
     // 1回の取得件数???
     const LIMIT_NUM = 50;
@@ -58,25 +57,45 @@ function getTimeline(props: TimeLineInfo){
     });
     agent.resumeSession(props.session);
 
-    // タイムラインを取得する
-
-    return;
+    // タイムラインを取得する 以下の変数は削除してください
+    const tmp : TimeLineProps = {tweets:[], tweets_num: 5}; 
+    return tmp;
 }
 
-function TimelineForward(props: {load_num: number, setLoadNum: (num: number) => void}) : JSX.Element {
-    //出来るかな～　不明　おらわくわくしてきたぞ！！！！
+// 下方向の無限スクロールを実装するコンポーネント
+export function Scroll(props: TimeLineInfo): JSX.Element {
+    const [list, setList] = useState<JSX.Element[]>([]);
+
+    // 項目を読み込むときのコールバック
+    const loadMore = () => {
+        // ここでタイムラインのデータを非同期で取得する処理を実装
+        // Timelineを取得し、pageに格納
+        const page = Timeline(props)
+        // 取得したデータをリストに追加
+        setList([...list, page]);
+    };
+
+    // 下方向の無限スクロールの実装
     return (
-        <div>
-        </div>
+        <InfiniteScroll
+            // 初期のページは0
+            pageStart={0}
+            // スクロールすると呼ばれる関数
+            loadMore={loadMore}
+            // まだ読み込むアイテムがあるかどうか
+            hasMore={true}
+            // ローディング中に表示するコンポーネント
+            loader={<div className="loader" key={0}>Loading ...</div>}    
+        >   
+            {/* リストの中身を表示*/}   
+            {list.map(
+                (value) => (
+                    <p>{value}</p>
+                )
+            )}
+        </InfiniteScroll>
     )
 }
 
-function TimelineBackward(props: {load_num: number, setLoadNum: (num: number) => void}) : JSX.Element {
-    //出来るかどうか不明
-    return (
-        <div>
-        </div>
-    )
-}
 
 export default Timeline;
